27215e4e6d780fe590bf7d564106e0a405d16b4e
manoj <manojavhale@gmail.com>
2024-07-01 22:39:23 +0530

    App Setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@contexts': './src/contexts',
+          '@screens': './src/screens',
+          '@styles': './src/styles',
+          '@routes': './src/routes',
+          '@services': './src/services',
+          '@hooks': './src/hooks',
+          '@shared': './src/shared',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
