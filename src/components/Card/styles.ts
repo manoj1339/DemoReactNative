6ecb9b28505710f39fff7ce9f102425856c3f14b
manoj <manojavhale@gmail.com>
2024-07-03 10:47:49 +0530

    Config Changes

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,63 @@
+import {WINDOW_WIDTH} from '@utils/constants';
+import {StyleSheet} from 'react-native';
+
+const CARD_BORDER_RADIUS = 16;
+
+export const styles = StyleSheet.create({
+    container: {
+        width: WINDOW_WIDTH * 0.9,
+        borderRadius: CARD_BORDER_RADIUS,
+        overflow: 'hidden',
+    },
+    headerText: {
+        fontSize: 18,
+        fontFamily: 'SpaceGrotesk',
+        fontWeight: 'bold',
+        color: '#fff',
+    },
+    upperSection: {
+        width: WINDOW_WIDTH * 0.9,
+        padding: 16,
+    },
+    chipContactlessContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 16,
+    },
+    smallImage: {
+        width: 50,
+        height: 50,
+    },
+    cardNumber: {
+        fontSize: 24,
+        color: '#fff',
+    },
+    lowerSection: {
+        width: WINDOW_WIDTH * 0.9,
+        padding: 16,
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderTopWidth: 1,
+        borderTopColor: '#fff',
+    },
+    labelText: {
+        fontSize: 10,
+        color: '#fff',
+        marginBottom: 7,
+    },
+    labelValue: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
+    firstBox: {
+        flex: 3,
+    },
+    secondBox: {
+        flex: 2,
+    },
+    thirdBox: {
+        flex: 1,
+    },
+});
---
+++ src/images/index.ts
@@ -0,0 +1,9 @@
+export const IMAGES = {
+    card_one_bg: require('./card_one_bg.png'),
+    card_two_bg: require('./card_two_bg.png'),
+    card_three_bg: require('./card_three_bg.png'),
+    card_four_bg: require('./card_four_bg.png'),
+    chip: require('./chip.png'),
+    contactless: require('./contactless.png'),
+    vendor: require('./vendor.png'),
+};
